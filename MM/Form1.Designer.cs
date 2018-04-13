15484d980202b20b0492421c64ef1f8741b7e5ae
titu84 <krzysztof.sauermann@gmail.com>
2018-04-14 00:45:41 +0200

    .

---
+++ MM/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace MM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col1,
+            this.Col2,
+            this.Col3,
+            this.Col4,
+            this.Col5});
+            this.dgv.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgv.Enabled = false;
+            this.dgv.Location = new System.Drawing.Point(44, 62);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(469, 284);
+            this.dgv.TabIndex = 0;
+            // 
+            // col1
+            // 
+            this.col1.HeaderText = "Kolor1";
+            this.col1.Name = "col1";
+            this.col1.Width = 84;
+            // 
+            // Col2
+            // 
+            this.Col2.HeaderText = "Kolor2";
+            this.Col2.Name = "Col2";
+            this.Col2.Width = 84;
+            // 
+            // Col3
+            // 
+            this.Col3.HeaderText = "Kolor3";
+            this.Col3.Name = "Col3";
+            this.Col3.Width = 84;
+            // 
+            // Col4
+            // 
+            this.Col4.HeaderText = "Kolor4";
+            this.Col4.Name = "Col4";
+            this.Col4.Width = 84;
+            // 
+            // Col5
+            // 
+            this.Col5.HeaderText = "Wynik";
+            this.Col5.Name = "Col5";
+            this.Col5.Width = 85;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "r",
+            "y",
+            "g",
+            "b",
+            "m",
+            "c"});
+            this.comboBox1.Location = new System.Drawing.Point(85, 365);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(82, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.BackColorChanged += new System.EventHandler(this.comboBox1_BackColorChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "r",
+            "y",
+            "g",
+            "b",
+            "m",
+            "c"});
+            this.comboBox2.Location = new System.Drawing.Point(173, 365);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(84, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.BackColorChanged += new System.EventHandler(this.comboBox2_BackColorChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "r",
+            "y",
+            "g",
+            "b",
+            "m",
+            "c"});
+            this.comboBox3.Location = new System.Drawing.Point(263, 365);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(84, 21);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            this.comboBox3.BackColorChanged += new System.EventHandler(this.comboBox3_BackColorChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "r",
+            "y",
+            "g",
+            "b",
+            "m",
+            "c"});
+            this.comboBox4.Location = new System.Drawing.Point(353, 365);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(84, 21);
+            this.comboBox4.TabIndex = 4;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            this.comboBox4.BackColorChanged += new System.EventHandler(this.comboBox4_BackColorChanged);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(444, 365);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(69, 23);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "Sprawdź";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Nowa Gra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(415, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Ilość prób:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(477, 26);
+            this.maskedTextBox1.Mask = "0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(36, 20);
+            this.maskedTextBox1.TabIndex = 13;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 427);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Master Mind";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ MM/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        char[] colors = new char[] { 'r', 'y', 'g', 'b', 'm', 'c' };
+        char[] secret = new char[4];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "0009";
+            addSecret();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            // Dodanie próby do DataGrida
+            dgv.Rows.Add(comboBox1.Text, comboBox2.Text, comboBox3.Text, comboBox4.Text);
+
+            //Sprawdzanie trafień
+            bool?[] hits = getHits(new char[] { comboBox1.Text.ToChar(), comboBox2.Text.ToChar(), comboBox3.Text.ToChar(), comboBox4.Text.ToChar() });
+           
+            string hitsStr = "";
+            for (int i = 0; i < hits.Where(a => a.HasValue && a.Value == true).Count(); i++)
+            {
+                hitsStr += "•";
+            }
+            for (int i = 0; i < hits.Where(a => a.HasValue && a.Value == false).Count(); i++)
+            {
+                hitsStr += "○";
+            }
+            //kolorki w gridzie
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                row.Cells[0].Style.BackColor = getColor(row.Cells[0].Value.ToString());
+                row.Cells[1].Style.BackColor = getColor(row.Cells[1].Value.ToString());
+                row.Cells[2].Style.BackColor = getColor(row.Cells[2].Value.ToString());
+                row.Cells[3].Style.BackColor = getColor(row.Cells[3].Value.ToString());                
+            }
+            int lastRow = dgv.Rows.GetLastRow(DataGridViewElementStates.Visible); // Index ostatniego row w Gridzie
+            dgv.Rows[lastRow].Cells[4].Value = hitsStr; // Dopisanie kropek wskazujących ilość poprawnych odpowiedzi
+            dgv.ClearSelection();
+            if (hitsStr == "••••")
+            {
+                MessageBox.Show("Gratulacje wygrałeś!!!");
+                addSecret();                
+                return;
+            }
+            if (dgv.RowCount == int.Parse(maskedTextBox1.Text))
+            {
+                MessageBox.Show("Przegrałeś, koniec gry!\n Sekretny kod to: " + string.Join("_", secret));
+                addSecret();               
+            } 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBoxColor(comboBox1);
+        }       
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBoxColor(comboBox2);
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBoxColor(comboBox3);
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBoxColor(comboBox4);
+        }
+        private void comboBoxColor(ComboBox cb)
+        {
+            cb.BackColor = getColor(cb.SelectedItem.ToString());
+        }
+        private Color getColor(string text)
+        {
+            switch (text)
+            {
+                case "r":
+                    return Color.Red;
+                case "y":
+                    return Color.Yellow;
+                case "g":
+                    return Color.Green;
+                case "b":
+                    return Color.LightBlue;
+                case "m":
+                    return Color.Magenta;
+                case "c":
+                    return Color.Cyan;
+                default:
+                    return Color.White;
+            }                   
+        }
+
+        private void comboBox1_BackColorChanged(object sender, EventArgs e)
+        {
+            SendKeys.Send("{TAB}");
+        }
+
+        private void comboBox2_BackColorChanged(object sender, EventArgs e)
+        {
+            SendKeys.Send("{TAB}");
+        }
+
+        private void comboBox3_BackColorChanged(object sender, EventArgs e)
+        {
+            SendKeys.Send("{TAB}");
+        }
+
+        private void comboBox4_BackColorChanged(object sender, EventArgs e)
+        {
+            SendKeys.Send("{TAB}");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {                       
+            addSecret();            
+        }
+
+        private void addSecret()
+        {           
+            Random rnd = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                int index = rnd.Next(5);
+                secret[i] = colors[index];
+            }
+            dgv.Rows.Clear();
+        }
+        private bool?[] getHits(char[] input)
+        {
+            bool?[] result = new bool?[4];
+            // null  - brak trafienia
+            // false - trafienie ale na złym polu
+            // true  - trafienie pełne
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (secret[i] == input[i])
+                {
+                    result[i] = true;                   
+                }
+                else if (secret[i] != input[i] && secret.Contains(input[i]))
+                {
+                    result[i] = false;
+                }
+                else
+                {
+                    result[i] = null;
+                }
+            }
+            return result;
+        }
+    }
+    public static class Extensions
+    {
+        public static char ToChar(this string value)
+        {
+            return Convert.ToChar(value);
+        }
+    }
+}
